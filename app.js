a353c4392c31cfc20bd9f24321fbb10fe5c6dd6a
Kaleb <twitcherc@gmail.com>
2019-06-16 13:57:48 -0600

    updoot

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/userManagement', {useNewUrlParser: true}); // "userManagement" is the db name
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost/appusers", { useNewUrlParser: true }); // "userManagement" is the db name
 const db = mongoose.connection;
 const userSchema = new mongoose.Schema({
-   name: String,
-   role: String,
-   age: { type: Number, min: 18, max: 70 },
-   createdDate: { type: Date, default: Date.now }
+  name: String,
+  role: String,
+  age: { type: Number, min: 18, max: 70 },
+  createdDate: { type: Date, default: Date.now }
 });
 
-const user = mongoose.model('userCollection', userSchema);
+const user = mongoose.model("userCollection", userSchema);
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('db connected');
+//  db.on('error', console.error.bind(console, 'connection error:'));
+//  db.once('open', function () {
+//      console.log('db connected');
+//      const newUser = new user();
+//      newUser.name = "Ken Adams";
+//      newUser.role = "Student";
+//      newUser.age = 20;
+//      newUser.save((err, data) => { // stored to the database
+//         if (err) {
+//             return console.error(err);
+//         }
+//         console.log(`new user save: ${data}`);
+//     });
+// });
+
+//Create -- new document
+
+app.post("/newUser", (req, res) => {
+  console.log(`POST /newUser: ${JSON.stringify(req.body)}`);
   const newUser = new user();
-  newUser.name = "Joe Mongoose";
-  newUser.role = "Admin";
-  newUser.save((err, data) => { // stored to the database
-      if (err) {
-          return console.error(err);
-      }
-      console.log(`new user save: ${data}`);
+  newUser.name = req.body.name;
+  newUser.role = req.body.role;
+  newUser.save((err, data) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log(`new user save: ${data}`);
+    res.send(`done ${data}`);
   });
+}); //
+
+app.listen(port, err => {
+  if (err) console.log(err);
+  console.log(`App Server listen on port: ${port}`);
 });
